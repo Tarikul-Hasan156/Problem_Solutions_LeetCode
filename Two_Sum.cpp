97849c1a051ce07097fd18279b23e798cbcca1ac
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-13 05:52:36 +0600

    Add files via upload

---
+++ Two_Sum.cpp
@@ -0,0 +1,51 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+    unordered_set<ll>s;
+    ll x,target=0;
+    while (cin>>x){s.insert(x);target=x;}
+    vector<ll>vv(s.begin(),s.end());
+    vv.pop_back();
+    sort(vv.begin(),vv.end());//  1 2 7 11 15
+    ll l=0,r=vv.size()-1;
+    while (l<r)
+    {
+        ll res=vv[l]+vv[r];
+        if (res==target){cout<<l<<sp<<r<<endl;return;}
+        else if (res<target)l++;
+        else r--;
+    }
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
